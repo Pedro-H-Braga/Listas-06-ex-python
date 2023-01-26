8e555bf75177aa20d36f4eb8636011b9ef08dfff
Pedro-H-Braga <pedrobragalima@gmail.com>
2023-01-26 19:56:51 -0300

    update

--- script03.py
+++ script03.py
@@ -15,11 +15,18 @@ valores_randomicos = range(1,100)
 repeticoes         = 0
 numero_randomicos  = []
 lista_ordenada     = []
+
 # condição para ser positivo e estar entre 0 a 9
 if dimensao > 0:
     # laço que popula a lista com valores aleatorios
     for i in range(dimensao):
         numero_randomicos.append(choice(valores_randomicos))
+        valor_lista_original = numero_randomicos[i]
+        posicao_lista_original = i
+        
+        if  valor_lista_original <:
+            lista_ordenada.append(numero_randomicos[i])
+
     print(f'LISTA ORIGINAL: <{numero_randomicos}>')
 
     for posicao_anterior,j in enumerate(numero_randomicos):
