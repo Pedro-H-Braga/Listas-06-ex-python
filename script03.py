c7add2cf05fea2ee5891871fe21e2d7c21ece27c
Pedro-H-Braga <pedrobragalima@gmail.com>
2023-01-26 12:56:56 -0300

    update

---
+++ script03.py
@@ -0,0 +1,37 @@
+'''
+Fazer um programa para gerar automaticamente uma lista de dimensão de n elementos (n deverá ser
+solicitado ao usuário e ser positivo), com os elementos na faixa dos números inteiros entre 0 e 99
+(inclusive), gerados aleatoriamente. Imprimir a lista original gerada, em seguida, ordená-la de forma
+crescente (usar laço de repetição) e imprimi-la. LEMBRANDO: NÃO USAR SORT() NEM SORTED()
+
+
+'''
+from random import choice
+
+print('<<Informar a baixo numeros inteiros e positivos>>')
+dimensao = int(input('Informe a dimensao da lista: '))
+
+valores_randomicos = range(1,100)
+repeticoes         = 0
+numero_randomicos  = []
+lista_ordenada     = []
+# condição para ser positivo e estar entre 0 a 9
+if dimensao > 0:
+    # laço que popula a lista com valores aleatorios
+    for i in range(dimensao):
+        numero_randomicos.append(choice(valores_randomicos))
+    print(f'LISTA ORIGINAL: <{numero_randomicos}>')
+
+    for posicao_anterior,j in enumerate(numero_randomicos):
+        if posicao_anterior == 0:
+            posicao_anterior += 1
+        posicao_anterior -= 1
+        numero_anterior = numero_randomicos[posicao_anterior]
+        if j < numero_anterior:
+            lista_ordenada.insert(posicao_anterior, j)
+        else:
+            lista_ordenada.append(j)
+        
+        print(f'LISTA ORDENADA: <{lista_ordenada}>')
+else: 
+    print('<<VALOR INFORMADO É INVÁLIDO>>')
