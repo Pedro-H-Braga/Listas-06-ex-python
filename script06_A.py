c91d84a8c1fd671ca444f99f4837ca6fbaa39a5b
Pedro-H-Braga <pedrobragalima@gmail.com>
2023-01-29 17:09:14 -0300

    up

--- script06_A.py
+++ script06_A.py
@@ -1,8 +1,7 @@
 '''
 Fazer um programa para gerar automaticamente uma lista de dimensão de n elementos (n deverá ser
 solicitado ao usuário e ser positivo), com os elementos na faixa dos números inteiros entre 0 e 99
-(inclusive), gerados aleatoriamente. Determinar (usar laço de repetição – NÃO USAR a biblioteca
-statistics.py):
+(inclusive), gerados aleatoriamente.
 
 a) A média dos valores dos elementos da lista
 soma dos elementos / dimensao da lista
@@ -15,12 +14,8 @@ print('<<Informar a baixo numeros inteiros e positivos>>')
 dimensao = int(input('Informe a dimensao da lista: '))
 
 valores_randomicos = range(1,100)
-somatorio          = float(0)
 media              = float(0)
-repeticoes         = 0
-
 numero_randomicos  = []
-lista_ordenada     = []
 
 # condição para ser positivo e estar entre 0 a 9
 if dimensao > 0:
